ab9c0ffd28b9447e22fcc7d1b763d342268828bf
Tripti Singh <71418879+Trptisingh@users.noreply.github.com>
2025-10-25 00:42:31 +0530

    Add files via upload

---
+++ Day3/script.js
@@ -0,0 +1,10 @@
+let car = {
+    color: "red", // key: value property
+    brand: "Toyota", // key: value property
+    year: 2020, // key: value property
+    start: function() { // method
+        console.log("Car started");
+    }
+};
+console.log(car.color); // Accessing property
+car.start(); // Calling method
